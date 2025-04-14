8878cda2b05baaf941b7a7e9c40459ccf607a9d2
krishnanx <abhishekkrishnan2006@gmail.com>
2025-04-14 19:18:06 +0530

    server reloads auto now

--- Server.py
+++ Server.py
@@ -126,4 +126,4 @@ async def stream_audio(request: Request):
     return StreamingResponse(generator, media_type="audio/mpeg", headers=headers)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7000, reload=False)
+    uvicorn.run("Server:app", host="0.0.0.0", port=7000, reload=True)
